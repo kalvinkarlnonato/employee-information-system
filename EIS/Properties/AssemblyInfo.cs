23b4e356d9db99cbfcbd8f01242f941712a0a7ed
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2023-09-17 19:56:09 +0800

    Add project files.

---
+++ EIS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EIS")]
+[assembly: AssemblyDescription("The Employee Information System (EIS) is a comprehensive and advanced digital platform designed to streamline and centralize all aspects of employee management within an organization. It serves as a powerful tool for HR departments, managers, and employees themselves, facilitating efficient and secure management of personnel-related data, tasks, and processes.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nonato")]
+[assembly: AssemblyProduct("EIS")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5824c327-749d-43d8-9e34-0f494da06f14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
